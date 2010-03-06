9642d04f15127c60790b6b98f03af1aa6da73435
Bruce Frederiksen <dangyogi@gmail.com>
2010-03-06 18:43:40 -0500

    First cut at wsgi_app.

---
+++ ucc/gui/controls/optional_ctrl.py
@@ -0,0 +1,59 @@
+# optional_ctrl.py
+
+'''Control for optional questions.'''
+
+import wx
+from ucc.gui.controls.base_ctrl import BaseCtrl
+from ucc.gui import debug
+
+class OptionalCtrl(BaseCtrl):
+    def __init__(self, subcls, *args, **kwargs):
+        self.subcls = subcls
+        super(OptionalCtrl, self).__init__(*args, **kwargs)
+    
+    def setupControl(self):
+        labelEnable = "Click to answer"
+        labelDisable = "Click to unanswer"
+        
+        self.cp = cp = wx.CollapsiblePane(self, label=labelEnable)
+        self.Bind(wx.EVT_COLLAPSIBLEPANE_CHANGED, self.onChange, cp)
+        
+        subpane = cp.GetPane()
+        self.subctrl = self.subcls(subpane, self.question, self.ans_getter, 
+                                   self.ans_setter)
+        
+        pane_sizer = wx.BoxSizer()
+        subpane.SetSizer(pane_sizer)
+        pane_sizer.Add(self.subctrl)
+        
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        self.SetSizer(sizer)
+        sizer.Add(cp, 1, wx.ALL|wx.EXPAND, 0)
+    
+    def setInitialValue(self):
+        debug.trace("%s.setInitialValue" % self.__class__.__name__)
+        
+        if self.get_value() is None:
+            debug.trace("Answer value is None")
+            self.cp.Collapse(True)
+        else:
+            debug.trace("Answer is not None")
+            self.cp.Expand()
+        
+        if self.cp.IsExpanded():
+            self.subctrl.setInitialValue()
+    
+    def onChange(self, event):
+        # self.set_value(str(event.GetInt()))
+        
+        debug.trace("Optional pane changed: %s" % self.cp.IsExpanded())
+        if self.cp.IsExpanded():
+            if self.get_answer() is None:
+                self.set_answer(self.question.make_default_answer())
+            self.subctrl.setInitialValue()
+            self.cp.SetLabel(self.label2)
+        else:
+            self.set_answer(None)
+            self.cp.SetLabel(self.label1)
+        self.parent.Layout()
+    
