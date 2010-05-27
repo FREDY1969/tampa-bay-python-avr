54173d545730fb59aff6819ae229ede878c4718f
Bruce Frederiksen <dangyogi@gmail.com>
2010-05-26 21:33:40 -0400

    Checkpoint reg_alloc.  Units tests pass.

--- ucc/codegen/reg_alloc.py
+++ ucc/codegen/reg_alloc.py
@@ -7,7 +7,7 @@ import itertools
 import collections
 
 from ucc.database import crud, triple2
-from ucc.codegen import code_seq, registers
+from ucc.codegen import code_seq
 
 def alloc_regs():
     subsets = get_reg_class_subsets()
@@ -43,36 +43,36 @@ def figure_out_multi_use(subsets, sizes):
             update triple_parameters
                set reg_class_for_parent =
                      (select csp.reg_class
-                        from triples t
-                               inner join code_seq_parameter csp
-                                 on t.code_seq_id = csp.code_seq_id
-                       where t.id = triple_parameters.parent_id
+                        from code_seq_parameter csp
+                       where triple_parameters.parent_code_seq_id =
+                               csp.code_seq_id
                          and triple_parameters.parameter_num =
                                csp.parameter_num),
                    num_regs_for_parent =
                      (select csp.num_registers
-                        from triples t
-                               inner join code_seq_parameter csp
-                                 on t.code_seq_id = csp.code_seq_id
-                       where t.id = triple_parameters.parent_id
+                        from code_seq_parameter csp
+                       where triple_parameters.parent_code_seq_id =
+                               csp.code_seq_id
                          and triple_parameters.parameter_num =
                                csp.parameter_num),
                    trashed =
                      (select csp.trashes
-                        from triples t
-                               inner join code_seq_parameter csp
-                                 on t.code_seq_id = csp.code_seq_id
-                       where t.id = triple_parameters.parent_id
+                        from code_seq_parameter csp
+                       where triple_parameters.parent_code_seq_id =
+                               csp.code_seq_id
                          and triple_parameters.parameter_num =
                                csp.parameter_num),
                    delink =
                      (select csp.delink
-                        from triples t
-                               inner join code_seq_parameter csp
-                                 on t.code_seq_id = csp.code_seq_id
-                       where t.id = triple_parameters.parent_id
+                        from code_seq_parameter csp
+                       where triple_parameters.parent_code_seq_id =
+                               csp.code_seq_id
                          and triple_parameters.parameter_num =
                                csp.parameter_num)
+             where parent_id not in (select t.id
+                                       from triples t
+                                      where t.operator in ('call_direct',
+                                                           'call_indirect'))
           ''')
 
     # This will handle the vast majority of triple_parameters:
@@ -81,6 +81,7 @@ def figure_out_multi_use(subsets, sizes):
             update triple_parameters
                set needed_reg_class = reg_class_for_parent
              where last_parameter_use
+               and reg_class_for_parent notnull
           ''')
 
     with crud.db_transaction():
@@ -88,99 +89,115 @@ def figure_out_multi_use(subsets, sizes):
                      'parameter_id', 'parent_id', 'parameter_num',
                      'trashed', 'reg_class_for_parent', 'last_parameter_use',
                      order_by=('parameter_id', ('parent_seq_num', 'desc'))))
-        last_parameter = None
-        for i, row in enumerate(rows):
-            if row['parameter_id'] != last_parameter:
-                last_parameter = row['parameter_id']
-                next_rc = row['reg_class_for_parent']
-                assert row['last_parameter_use']
-            elif trashed:
-                crud.update('triple_parameters',
-                            {'parameter_id': row['parameter_id'], 
-                             'parent_id': row['parent_id'],
-                             'parameter_num': row['parameter_num']},
-                            needed_reg_class = next_rc,
-                            move_needed_to_parent = 1)
-            elif (next_rc, row['reg_class_for_parent']) in subsets:
-                next_rc = subsets[next_rc, row['reg_class_for_parent']]
-                crud.update('triple_parameters',
-                            {'parameter_id': row['parameter_id'], 
-                             'parent_id': row['parent_id'],
-                             'parameter_num': row['parameter_num']},
-                            needed_reg_class = next_rc)
-            else:
-                prior = i + 1
-                done = False
-                if prior < len(rows) and not rows[prior]['trashed']:
-                    p = rows[prior]
-                    subset = subsets.get((p['reg_class_for_parent'],
-                                          row['reg_class_for_parent']))
-                    if subset is not None:
-                        if sizes[subset] > sizes[next_rc]:
-                            next_rc = subset
+        for k, params \
+         in itertools.groupby(rows, key=lambda row: row['parameter_id']):
+            params = tuple(params)
+            for i, row in enumerate(params):
+                if i == 0:
+                    next_rc = row['reg_class_for_parent']
+                    assert row['last_parameter_use']
+                    if next_rc is None: break
+                elif row['trashed']:
+                    crud.update('triple_parameters',
+                                {'parameter_id': row['parameter_id'], 
+                                 'parent_id': row['parent_id'],
+                                 'parameter_num': row['parameter_num']},
+                                needed_reg_class = next_rc,
+                                move_needed_to_parent = 1)
+                elif row['reg_class_for_parent'] is None:
+                    break
+                elif (next_rc, row['reg_class_for_parent']) in subsets:
+                    next_rc = subsets[next_rc, row['reg_class_for_parent']]
+                    crud.update('triple_parameters',
+                                {'parameter_id': row['parameter_id'], 
+                                 'parent_id': row['parent_id'],
+                                 'parameter_num': row['parameter_num']},
+                                needed_reg_class = next_rc)
+                else:
+                    prior = i + 1
+                    done = False
+                    if prior < len(params):
+                        p = params[prior]
+                        if p['reg_class_for_parent'] is None \
+                           or p['trashed'] is None:
+                            break
+                        if not p['trashed']:
+                            subset = subsets.get((p['reg_class_for_parent'],
+                                                  row['reg_class_for_parent']))
+                            if subset is not None:
+                                if sizes[subset] > sizes[next_rc]:
+                                    next_rc = subset
+                                    crud.update('triple_parameters',
+                                      {'parameter_id': row['parameter_id'], 
+                                       'parent_id': row['parent_id'],
+                                       'parameter_num': row['parameter_num']},
+                                      needed_reg_class = next_rc)
+                                    crud.update('triple_parameters',
+                                      {'parameter_id': row['parameter_id'],
+                                       'parent_id': params[i - 1]['parent_id'],
+                                       'parameter_num':
+                                         params[i - 1]['parameter_num']},
+                                      move_prior_to_needed = 1)
+                                else:
+                                    next_rc = subset
+                                    crud.update('triple_parameters',
+                                      {'parameter_id': row['parameter_id'], 
+                                       'parent_id': row['parent_id'],
+                                       'parameter_num': row['parameter_num']},
+                                      needed_reg_class = next_rc,
+                                      move_needed_to_next = 1)
+                                done = True
+                            else:
+                                subset = subsets.get((p['reg_class_for_parent'],
+                                                      next_rc))
+                                if subset is not None:
+                                    next_rc = subset
+                                    crud.update('triple_parameters',
+                                      {'parameter_id': row['parameter_id'],
+                                       'parent_id': row['parent_id'],
+                                       'parameter_num': row['parameter_num']},
+                                      needed_reg_class = next_rc,
+                                      move_needed_to_parent = 1)
+                                    done = True
+                    if not done:
+                        if sizes[row['reg_class_for_parent']] > sizes[next_rc]:
+                            next_rc = row['reg_class_for_parent']
                             crud.update('triple_parameters',
-                                        {'parameter_id': row['parameter_id'], 
+                                        {'parameter_id': row['parameter_id'],
                                          'parent_id': row['parent_id'],
                                          'parameter_num': row['parameter_num']},
                                         needed_reg_class = next_rc)
                             crud.update('triple_parameters',
-                                        {'parameter_id': row['parameter_id'],
-                                         'parent_id': rows[i - 1]['parent_id'],
+                                        {'parameter_id': row['parameter_id'], 
+                                         'parent_id':
+                                           params[i - 1]['parent_id'],
                                          'parameter_num':
-                                           rows[i - 1]['parameter_num']},
+                                           params[i - 1]['parameter_num']},
                                         move_prior_to_needed = 1)
                         else:
-                            next_rc = subset
-                            crud.update('triple_parameters',
-                                        {'parameter_id': row['parameter_id'], 
-                                         'parent_id': row['parent_id'],
-                                         'parameter_num': row['parameter_num']},
-                                        needed_reg_class = next_rc,
-                                        move_needed_to_next = 1)
-                        done = True
-                    else:
-                        subset = subsets.get((p['reg_class_for_parent'],
-                                              next_rc))
-                        if subset is not None:
-                            next_rc = subset
                             crud.update('triple_parameters',
                                         {'parameter_id': row['parameter_id'],
                                          'parent_id': row['parent_id'],
                                          'parameter_num': row['parameter_num']},
                                         needed_reg_class = next_rc,
                                         move_needed_to_parent = 1)
-                            done = True
-                if not done:
-                    if sizes[row['reg_class_for_parent']] > sizes[next_rc]:
-                        next_rc = row['reg_class_for_parent']
-                        crud.update('triple_parameters',
-                                    {'parameter_id': row['parameter_id'],
-                                     'parent_id': row['parent_id'],
-                                     'parameter_num': row['parameter_num']},
-                                    needed_reg_class = next_rc)
-                        crud.update('triple_parameters',
-                                    {'parameter_id': row['parameter_id'], 
-                                     'parent_id': rows[i - 1]['parent_id'],
-                                     'parameter_num':
-                                       rows[i - 1]['parameter_num']},
-                                    move_prior_to_needed = 1)
-                    else:
-                        crud.update('triple_parameters',
-                                    {'parameter_id': row['parameter_id'],
-                                     'parent_id': row['parent_id'],
-                                     'parameter_num': row['parameter_num']},
-                                    needed_reg_class = next_rc,
-                                    move_needed_to_parent = 1)
 
     # Reset ghost flag for delinked triple_parameters:
     # FIX: This could violate a triple_order_constraint!
+    #      But I don't think that it will be cause it should only be
+    #      constant parameters that are delinked.
     with crud.db_transaction():
+        # Mark delinks as ghosts.
         crud.Db_cur.execute('''
             update triple_parameters
                set ghost = 1
              where ghost = 0 and delink
           ''')
 
+        # And pick another triple_parameter to evaluate the parameter triple.
+        # Note that if all triple_parameters are marked 'delink', then the
+        # parameter triple will not have any ghost = 0, so will not have code
+        # generated for it.
         crud.Db_cur.execute('''
             update triple_parameters
                set ghost = 0
@@ -213,19 +230,37 @@ def create_reg_map(subsets, sizes, code_seqs):
     # {(reg_class, reg_num): [(kind, kind_id, reg_class, reg_num), ...]}
     reg_map = {}
 
-    for symbol_id in get_functions():
-        do_reg_map_for_fun(symbol_id, subsets, sizes, code_seqs, reg_map)
+    for next_fn_layer in get_functions():
+        for symbol_id in next_fn_layer:
+            do_reg_map_for_fun(symbol_id, subsets, sizes, code_seqs, reg_map)
 
     write_reg_map(reg_map)
 
 def get_functions():
-    r'''Generates the symbol_id for functions in a bottom-up order.
+    r'''Yields sets of functions in a bottom-up order.
+
+    Each set is all of the functions that don't call any functions not yet
+    yielded.
+
+    Each function in the set is simply its symbol_id.
     '''
-    successors = collections.defaultdict(set)
+    fns = set()
+    calls = collections.defaultdict(set)
+    called_by = collections.defaultdict(set)
     for caller_id, called_id \
      in crud.read_as_tuples('fn_calls', 'caller_id', 'called_id', depth=1):
-        successors[called_id].add(caller_id)
-    return registers.topo_sort(successors.keys(), lambda id: successors[id])
+        fns.add(called_id)
+        fns.add(caller_id)
+        calls[caller_id].add(called_id)
+        called_by[called_id].add(caller_id)
+    while fns:
+        bottom = fns - calls.keys()
+        yield bottom
+        fns -= bottom
+        for fn in bottom:
+            for caller in called_by[fn]:
+                calls[caller].remove(fn)
+                if not calls[caller]: del calls[caller]
 
 def do_reg_map_for_fun(symbol_id, subsets, sizes, code_seqs, reg_map):
     # {(reg_class, reg_num): [(kind, kind_id, reg_class, reg_num), ...]}
@@ -238,11 +273,12 @@ def do_reg_map_for_fun(symbol_id, subsets, sizes, code_seqs, reg_map):
 
 def do_reg_map_for_block(id, symbol_id, subsets, sizes, code_seqs, reg_map):
     for triple in triple2.read_triples(id):
-        if t.use_count == 0:
+        if triple.use_count == 0:
             do_reg_map_for_triple(triple, subsets, sizes, code_seqs, reg_map)
 
 def do_reg_map_for_triple(triple, subsets, sizes, code_seqs, reg_map):
-    params = [do_reg_map_for_triple(param, subsets, sizes, code_seqs, reg_map)
+    params = [do_reg_map_for_triple(param.parameter, subsets, sizes, code_seqs,
+                                    reg_map)
               for param in triple.children]
 
 def write_reg_map(reg_map):
