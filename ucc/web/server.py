f0f315a0d430ea10496273bebf1d8c607efad7fe
Jason Keene <jasonkeene@gmail.com>
2010-06-26 00:17:37 -0400

    Just some general cleanup to refamiliarize myself with the project.

--- ucc/web/server.py
+++ ucc/web/server.py
@@ -1,17 +1,21 @@
 # server.py
 
-from doctest_tools import setpath
-setpath.setpath(__file__, remove_first = True)
+"""Functions for controlling a WSGI application server"""
 
-from wsgiref.simple_server import make_server
 import webbrowser
-from ucc.web.wsgi_app import wsgi_app
+from wsgiref.simple_server import make_server
 
-def start(port=8005):
-    httpd = make_server('', port, wsgi_app)
-    print("Serving HTTP on port {}...".format(port))
-    
-    webbrowser.open('http://localhost:{}'.format(port), 2)
-    
-    # Respond to requests until process is killed
-    httpd.serve_forever()
+WSGI_SERVER = None
+
+def start(app, host='', port=8005):
+    """Start the WSGI application."""
+    WSGI_SERVER = make_server(host, port, app)
+    url = 'http://{server_name}:{server_port}/'.format(**WSGI_SERVER.__dict__)
+    print("Serving WSGI application on {}...".format(url))
+    webbrowser.open(url, 2)
+    WSGI_SERVER.serve_forever()
+
+def stop():
+    """Stop the WSGI application."""
+    if WSGI_SERVER is not None:
+        WSGI_SERVER.shutdown()
