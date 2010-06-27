c6edff4361237f795e1e084fe562a89d306eb69a
Jason Keene <jasonkeene@gmail.com>
2010-06-26 21:12:49 -0400

    Checkpoint on GUI.

--- ucc/web/media/components/jquery.ctrl.js
+++ ucc/web/media/components/jquery.ctrl.js
@@ -3,11 +3,11 @@
  */
 
 jQuery.ctrl = function(key, callback, args) {
-    $(document).keydown(function(e) {
-        if(!args) args=[]; // IE barks when args is null
-        if(e.keyCode == key.charCodeAt(0) && e.ctrlKey) {
-            callback.apply(this, args);
-            return false;
-        }
-    });
+	$(document).keydown(function(e) {
+		if(!args) args=[]; // IE barks when args is null
+		if(e.keyCode == key.charCodeAt(0) && e.ctrlKey) {
+			callback.apply(this, args);
+			return false;
+		}
+	});
 };
--- ucc/web/media/js/menu.js
+++ ucc/web/media/js/menu.js
@@ -1,1 +1,29 @@
 
+(function ($, ucc) {
+	ucc.menu = {};
+	ucc.menu.handlers = {
+		'open-package': function () {
+			
+		},
+		'save-current-word': function () {
+			
+		},
+		'compile-program': function () {
+			
+		},
+		'load-program': function () {
+			
+		}
+	};
+	
+	$(function () {
+		$('#menu a').click(function (e) {
+			var href = $(this).attr('href');
+			var hash_index = href.indexOf('#');
+			if (hash_index >= 0) {
+				var hash = href.slice(hash_index+1);
+				ucc.menu.handlers[hash]();
+			}
+		});
+	});
+})(jQuery, ucc);
