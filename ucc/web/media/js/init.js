2d669fb73008708aab440502ffd35020a62f9405
Jason Keene <jasonkeene@gmail.com>
2010-08-07 14:34:53 -0400

    Stuff let uncommited

--- ucc/web/media/js/init.js
+++ ucc/web/media/js/init.js
@@ -3,19 +3,20 @@
 
 var ucc = {};
 
-// handle resize window event
-
 (function ($) {
+	
+	// handle resize window event
+	
 	function resize() {
 		$('#package-tree').height($(window).height() - 26 - 38 - 10);
 		$('#word').height($(window).height() - 26 - 38);
 	}
 	$(resize);
 	$(window).resize(resize);
+	
+	// load in major gui components
+	
+	$.include('/js/tree.js');
+	$.include('/js/menu.js');
+	$.include('/js/words.js');
 })(jQuery);
-
-// load in major gui components
-
-$.include('/js/tree.js');
-$.include('/js/menu.js');
-$.include('/js/words.js');
